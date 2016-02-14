65000d7ddafef57e43d7d91ceb2a4cb6e0635e65
tarlepp <tarmo.leppanen@kase.fi>
2016-02-14 20:12:52 +0200

    Clean up.

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,11 @@
 'use strict';
 
 var gulp = require('gulp');
-var replace = require('gulp-replace-task');
 var fs = require('fs');
 var g = require('gulp-load-plugins')({lazy: false});
 var ngConstant = require('gulp-ng-constant');
 var noop = g.util.noop;
 var es = require('event-stream');
-var queue = require('streamqueue');
 var lazypipe = require('lazypipe');
 var stylish = require('jshint-stylish');
 var bower = require('./bower');
@@ -24,9 +22,7 @@ var htmlminOpts = {
   removeRedundantAttributes: true
 };
 
-/**
- * JS Hint
- */
+// JS Hint
 gulp.task('jshint', function() {
   return gulp.src([
     './gulpfile.js',
@@ -37,9 +33,7 @@ gulp.task('jshint', function() {
   .pipe(livereload());
 });
 
-/**
- * CSS
- */
+// CSS
 gulp.task('clean-css', function() {
   return gulp.src('./.tmp/css').pipe(g.clean());
 });
@@ -60,7 +54,9 @@ gulp.task('styles-dist', ['styles'], function() {
 });
 
 gulp.task('csslint', ['styles'], function() {
-  return cssFiles()
+  return gulp.src([
+      './src/app/**/*.css'
+    ])
     .pipe(g.cached('csslint'))
     .pipe(g.csslint('./.csslintrc'))
     .pipe(g.csslint.reporter())
@@ -76,16 +72,12 @@ gulp.task('scsslint', ['styles'], function() {
     }));
 });
 
-/**
- * Scripts
- */
+// Scripts
 gulp.task('scripts-dist', ['templates-dist'], function() {
   return appFiles().pipe(dist('js', bower.name, {ngAnnotate: true}));
 });
 
-/**
- * Templates
- */
+// Templates
 gulp.task('templates', function() {
   return templateFiles().pipe(buildTemplates());
 });
@@ -94,9 +86,7 @@ gulp.task('templates-dist', function() {
   return templateFiles({min: true}).pipe(buildTemplates());
 });
 
-/**
- * Vendors
- */
+// Vendors
 gulp.task('vendors', function() {
   var bowerStream = mainBowerFiles();
 
@@ -106,9 +96,7 @@ gulp.task('vendors', function() {
   );
 });
 
-/**
- * Index
- */
+// Index
 gulp.task('index', index);
 gulp.task('build-all', ['styles', 'templates'], index);
 
@@ -124,36 +112,28 @@ function index() {
   ;
 }
 
-/**
- * Assets
- */
+// Assets
 gulp.task('assets', function() {
   return gulp.src('./src/app/assets/**')
     .pipe(gulp.dest('./dist/assets'))
   ;
 });
 
-/**
- * Partials
- */
+// Partials
 gulp.task('partials', function() {
   return gulp.src('./src/app/partials/**')
     .pipe(gulp.dest('./dist/partials'))
   ;
 });
 
-/**
- * Fonts
- */
+// Fonts
 gulp.task('fonts', function() {
   return gulp.src('./bower_components/fontawesome/fonts/**')
     .pipe(gulp.dest('./dist/fonts'))
   ;
 });
 
-/**
- * Dist
- */
+// Dist
 gulp.task('dist', ['vendors', 'assets', 'fonts', 'styles-dist', 'scripts-dist'], function() {
   return gulp.src('./src/app/index.html')
     .pipe(g.inject(gulp.src('./dist/vendors.min.{js,css}'), {
@@ -166,27 +146,21 @@ gulp.task('dist', ['vendors', 'assets', 'fonts', 'styles-dist', 'scripts-dist'],
   ;
 });
 
-/**
- * Static file server
- */
+// Static file server
 gulp.task('statics', g.serve({
   port: 3000,
   root: ['./.tmp', './src/app', './bower_components'],
   middleware: historyApiFallback({})
 }));
 
-/**
- * Production file server, note remember to run 'gulp dist' first!
- */
+// Production file server, note remember to run 'gulp dist' first!
 gulp.task('production', g.serve({
   port: 3001,
   root: ['./dist'],
   middleware: historyApiFallback({})
 }));
 
-/**
- * Watch
- */
+// Watch
 gulp.task('serve', ['config', 'watch']);
 
 gulp.task('watch', ['statics', 'default'], function() {
@@ -221,39 +195,18 @@ gulp.task('config', function() {
     .pipe(gulp.dest('./src/app/config/'));
 });
 
-/**
- * Default task
- */
+// Default task
 gulp.task('default', ['lint', 'build-all']);
 
-/**
- * Lint everything
- */
+// Lint everything
 gulp.task('lint', ['jshint', 'csslint', 'scsslint']);
 
-/**
- * Test files
- */
-function testFiles() {
-  return new queue({objectMode: true})
-    .queue(gulp.src(mainBowerFiles()).pipe(g.filter('**/*.js')))
-    .queue(gulp.src('./bower_components/angular-mocks/angular-mocks.js'))
-    .queue(appFiles())
-    .queue(gulp.src('./src/app/**/*_test.js'))
-    .done()
-  ;
-}
-
-/**
- * All CSS files as a stream
- */
+// All CSS files as a stream
 function cssFiles(opt) {
   return gulp.src('./.tmp/css/**/*.css', opt);
 }
 
-/**
- * All AngularJS application files as a stream
- */
+// All AngularJS application files as a stream
 function appFiles() {
   var files = [
     './.tmp/' + bower.name + '-templates.js',
@@ -262,22 +215,16 @@ function appFiles() {
   ];
 
   return gulp.src(files)
-    .pipe(g.angularFilesort())
-  ;
+    .pipe(g.angularFilesort());
 }
 
-/**
- * All AngularJS templates/partials as a stream
- */
+// All AngularJS templates/partials as a stream
 function templateFiles(opt) {
   return gulp.src(['./src/app/**/*.html', '!./src/app/index.html'], opt)
-    .pipe(opt && opt.min ? g.htmlmin(htmlminOpts) : noop())
-  ;
+    .pipe(opt && opt.min ? g.htmlmin(htmlminOpts) : noop());
 }
 
-/**
- * Build AngularJS templates/partials
- */
+// Build AngularJS templates/partials
 function buildTemplates() {
   return lazypipe()
     .pipe(g.ngHtml2js, {
@@ -287,8 +234,7 @@ function buildTemplates() {
     })
     .pipe(g.concat, bower.name + '-templates.js')
     .pipe(gulp.dest, './.tmp')
-    .pipe(livereload)()
-  ;
+    .pipe(livereload)();
 }
 
 /**
@@ -309,28 +255,21 @@ function dist(ext, name, opt) {
     .pipe(opt.ngAnnotate ? gulp.dest : noop, './dist')
     .pipe(ext === 'js' ? g.uglify : g.minifyCss)
     .pipe(g.rename, name + '.min.' + ext)
-    .pipe(gulp.dest, './dist')()
-  ;
+    .pipe(gulp.dest, './dist')();
 }
 
-/**
- * Livereload (or noop if not run by watch)
- */
+// Livereload (or noop if not run by watch)
 function livereload() {
   return lazypipe()
-    .pipe(isWatching ? g.livereload : noop)()
-  ;
+    .pipe(isWatching ? g.livereload : noop)();
 }
 
-/**
- * Jshint with stylish reporter
- */
+// Jshint with stylish reporter
 function jshint(jshintfile) {
   // Read JSHint settings, for some reason jshint-stylish won't work on initial load of files
   var jshintSettings = JSON.parse(fs.readFileSync(jshintfile, 'utf8'));
 
   return lazypipe()
     .pipe(g.jshint, jshintSettings)
-    .pipe(g.jshint.reporter, stylish)()
-  ;
+    .pipe(g.jshint.reporter, stylish)();
 }
