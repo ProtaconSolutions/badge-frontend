bd26449407fbdc77dfc2144a7052099976bc0278
tarlepp <tarmo.leppanen@kase.fi>
2016-02-15 23:24:05 +0200

    Some small changes.

--- src/app/layout/layout.sidenav.controller.js
+++ src/app/layout/layout.sidenav.controller.js
@@ -20,16 +20,25 @@
    *
    * @param {*}                     $scope
    * @param {*}                     $localStorage
+   * @param {*}                     $mdSidenav
    * @param {Services.UserService}  UserService
    * @constructor
    */
   function SidenavController(
-    $scope, $localStorage,
+    $scope, $localStorage, $mdSidenav,
     UserService
   ) {
     var vm = this;
 
+    // Functions
+    vm.hideSideMenu = hideSideMenu;
+
     //////////
+
+    // Method to close sidenav
+    function hideSideMenu() {
+      $mdSidenav('left').close();
+    }
 
     $scope.$watch(function() {
       return angular.toJson($localStorage);
